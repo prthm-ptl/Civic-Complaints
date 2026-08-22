7bb47641a5c16b39ba7710396c335c21aab75826
Pratham <prathamp3009@gmail.com>
2026-08-22 18:15:40 +0530

    connected databse

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php 
+$con = mysqli_connect("localhost","root","","CivicComplaintsDB") or die("Connection Failed");
+session_start();
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" >
+    <input type="text" name="username_php" placeholder="username"/>
+    <input type="text" name="pswd_php" placeholder="password" />
+    <input type="submit" name="sub" />
+    
+</form>
+<?php
+    if(isset($_REQUEST["sub"]))
+        {
+            $ver_username = $_REQUEST["username_php"];
+            $ver_pswd = $_REQUEST["pswd_php"];
+
+            $q="SELECT * FROM `users` WHERE username='$ver_username'";
+            if($p=mysqli_query($con,$q))
+                {   
+                    if(mysqli_num_rows($p))
+                        {
+                            $array=mysqli_fetch_array($p);
+                            if($ver_username && password_verify($ver_pswd,$array['password']))
+                                {
+                                    $_SESSION['role']=$array['role'];
+                                    $_SESSION['user']=$array['name'];
+                                    header("location: feed.php");
+                                }
+                                else{
+                                    echo "username or password mismatch";
+                                }
+                        }
+                    else{
+                        echo "username or password mismatch";
+                    }
+                }
+        }
+?>
