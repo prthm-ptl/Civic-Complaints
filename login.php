07544b56c6f1fac29e8a9eb8a4f20ac8be9e2303
Pratham <prathamp3009@gmail.com>
2026-08-24 00:28:33 +0530

    improved login and signup process

--- login.php
+++ login.php
@@ -4,8 +4,9 @@ session_start();
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" >
     <input type="text" name="username_php" placeholder="username"/>
-    <input type="text" name="pswd_php" placeholder="password" />
-    <input type="submit" name="sub" />
+    <input type="password" name="pswd_php" placeholder="password" />
+    <input type="submit" name="sub" value="submit"/>
+    <input type="submit" name="sign_up" value="Sign Up"  />
     
 </form>
 <?php
@@ -22,6 +23,7 @@ session_start();
                             $array=mysqli_fetch_array($p);
                             if($ver_username && password_verify($ver_pswd,$array['password']))
                                 {
+                                    $_SESSION['userid']=$array['user_id'];
                                     $_SESSION['role']=$array['role'];
                                     $_SESSION['user']=$array['name'];
                                     header("location: feed.php");
@@ -34,5 +36,11 @@ session_start();
                         echo "username or password mismatch";
                     }
                 }
+        }
+    
+    if(isset($_REQUEST["sign_up"]))
+        {
+            $_SESSION['signup'] = 1;
+            header("location: signup.php");
         }
 ?>
--- signup.php
+++ signup.php
@@ -1,19 +1,25 @@
 <?php
 session_start();
 $con=mysqli_connect("localhost","root","","CivicComplaintsDB");
+if(!isset($_SESSION['signup']))
+{
+    echo "invalid access";
+}
+else{
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" >
     <input type="text" name="name" placeholder="your name" pattern="[a-zA-Z]+" required/>
     <input type="text" name="username_php" placeholder="username" pattern="[a-zA-Z0-9]+" required/>
     <input type="password" name="pswd_php" placeholder="password" required/>
     <select name="role_php" >
-        <option>citizen</option>
-        <option>officer</option>
+        <option value="citizen" selected>citizen</option>
+        <option value="officer">officer</option>
     </select>
     <input type="submit" name="sub"/>
 </form>
 
 <?php
+    echo "<a href='login.php'>login insted</a>";
                    
     if(isset($_REQUEST['sub']))
         {
@@ -39,8 +45,10 @@ $con=mysqli_connect("localhost","root","","CivicComplaintsDB");
                             $array=mysqli_fetch_array($p);
                             if($ver_username && password_verify($ver_pswd,$array['password']))
                                 {
+                                    $_SESSION['userid']=$array['user_id'];
                                     $_SESSION['role']=$array['role'];
                                     $_SESSION['user']=$array['name'];
+                                    unset($_SESSION['signup']);
                                     header("location: feed.php");
                                 }
                                 else{
@@ -52,4 +60,5 @@ $con=mysqli_connect("localhost","root","","CivicComplaintsDB");
                     }
                 }
         }
+}
 ?>
